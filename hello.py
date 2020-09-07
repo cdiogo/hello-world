bd7da9097ec750ae7a12707800c9adf1ab80d021
cdiogo <cdiogo.dias@gmail.com>
2020-09-07 16:52:15 -0300

    simple hello world in python using flask

---
+++ hello.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+
+    return_msg = {"message": "Hello World", 
+                  "hostname": str(socket.gethostname())
+                  }
+
+    return jsonify(return_msg)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
